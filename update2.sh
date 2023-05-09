8c69c917bdfb2d167646059575b49765154e471b
GyojinHwang <jason.hwang@designxplay.com>
2023-05-09 17:51:47 +0900

    dfdf

--- update2.sh
+++ update2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mv ./docker-compose_server.yml ./docker-compose.yml
-mv ./server/src/data-source_server.ts ./server/src/data-source.ts
+cp ./docker-compose_server.yml ./docker-compose.yml
+cp ./server/src/data-source_server.ts ./server/src/data-source.ts
